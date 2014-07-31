c10b9e2c6800d67a5038f662dd500771c7e9c8e4
Andy Balaam <andybalaam@artificialworlds.net>
2014-07-31 07:50:30 +0100

    Load animations from file

--- rabbit-escape-ui-swing/src/rabbitescape/ui/swing/AnimationTester.java
+++ rabbit-escape-ui-swing/src/rabbitescape/ui/swing/AnimationTester.java
@@ -1,35 +1,94 @@
 package rabbitescape.ui.swing;
 
+import rabbitescape.engine.err.RabbitEscapeException;
 import rabbitescape.render.Renderer;
 import rabbitescape.render.Sprite;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferStrategy;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.*;
+import java.util.List;
 
 import static rabbitescape.engine.i18n.Translation.t;
 
 public class AnimationTester extends JFrame
 {
+    private static class AnimationNotFound extends RabbitEscapeException
+    {
+        public final String name;
+
+        public AnimationNotFound( String name )
+        {
+            this.name = name;
+        }
+    }
+
+    private class Listener implements java.awt.event.MouseListener
+    {
+        @Override
+        public void mouseClicked( MouseEvent mouseEvent )
+        {
+            int i = screen2index( mouseEvent.getX(), mouseEvent.getY() );
+
+            Object[] possibilities = { "<none>", "walk", "bash" };
+            String changedAnimation = (String)JOptionPane.showInputDialog(
+                null,
+                "Choose animation:",
+                "Change animation",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                possibilities,
+                animations[i]
+            );
+
+            if ( changedAnimation.equals( "<none>") )
+            {
+                changedAnimation = "";
+            }
+
+            animations[i] = changedAnimation;
+            loadBitmaps();
+        }
+
+        @Override
+        public void mousePressed( MouseEvent mouseEvent )
+        {
+        }
+
+        @Override
+        public void mouseReleased( MouseEvent mouseEvent )
+        {
+        }
+
+        @Override
+        public void mouseEntered( MouseEvent mouseEvent )
+        {
+        }
+
+        @Override
+        public void mouseExited( MouseEvent mouseEvent )
+        {
+        }
+    }
+
     private final int tileSize;
+    private final int numTilesX;
     boolean running;
 
-    private final Canvas canvas;
+    private final java.awt.Canvas canvas;
     private SwingBitmapScaler scaler;
     private SwingPaint paint;
-    private SwingBitmap[] frames;
+    private SwingBitmap[][] frames;
 
-    private String[] walk = new String[] {
-        "rabbit-walk-01",
-        "rabbit-walk-02",
-        "rabbit-walk-03",
-        "rabbit-walk-04",
-        "rabbit-walk-05",
-        "rabbit-walk-06",
-        "rabbit-walk-07",
-        "rabbit-walk-08",
-        "rabbit-walk-09",
-        "rabbit-walk-10"
+    private String[] animations = new String[] {
+        "", "", "",
+        "walk", "walk", "bash",
+        "", "", ""
     };
 
     private static class InitUi implements Runnable
@@ -62,20 +121,22 @@ public class AnimationTester extends JFrame
     public AnimationTester()
     {
         this.tileSize = 128;
-        int numTilesX = 3;
+        this.numTilesX = 3;
         int numTilesY = 3;
 
         setIgnoreRepaint( true );
         setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 
-        this.canvas = new Canvas();
+        this.canvas = new java.awt.Canvas();
         canvas.setIgnoreRepaint( true );
         canvas.setPreferredSize(
-            new Dimension( tileSize * numTilesX, tileSize * numTilesY) );
+            new java.awt.Dimension( tileSize * numTilesX, tileSize * numTilesY) );
 
-        getContentPane().add(canvas, BorderLayout.CENTER);
+        getContentPane().add( canvas, java.awt.BorderLayout.CENTER );
 
         loadBitmaps();
+
+        canvas.addMouseListener(new Listener());
 
         setTitle( t( "Animation Tester" ) );
         pack();
@@ -91,33 +152,62 @@ public class AnimationTester extends JFrame
         paint = new SwingPaint();
         SwingBitmapLoader bitmapLoader = new SwingBitmapLoader();
 
-        frames = loadFrames( bitmapLoader, walk );
-
-        frames = new SwingBitmap[] {
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-01.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-02.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-03.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-04.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-05.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-06.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-07.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-08.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-09.png" ),
-            bitmapLoader.load(
-                "/rabbitescape/ui/swing/images32/rabbit-walk-10.png" )
-        };
+        frames = loadAllFrames( bitmapLoader, animations );
     }
 
-    private SwingBitmap[] loadFrames(SwingBitmapLoader bitmapLoader, String[] animation)
+    private SwingBitmap[][] loadAllFrames( SwingBitmapLoader bitmapLoader, String[] animations )
+    {
+        SwingBitmap[][] ret = new SwingBitmap[animations.length][];
+        int i = 0;
+        for ( String animation : animations )
+        {
+            if ( !animation.isEmpty() )
+            {
+                ret[i] = loadFrames(
+                    bitmapLoader, loadAnimation( animation ) );
+            }
+            ++i;
+        }
+        return ret;
+    }
+
+    private String[] loadAnimation( String name )
+    {
+        try
+        {
+            String key = "/rabbitescape/render/animations/" + name + ".rea";
+
+            URL url = getClass().getResource( key );
+            if ( url == null )
+            {
+                System.out.println( key + " not found." );
+                throw new AnimationNotFound( name );
+            }
+
+            BufferedReader reader = new BufferedReader(
+                new InputStreamReader( url.openStream() ) );
+
+            List<String> ret = new ArrayList<>();
+            String ln;
+            while ( ( ln = reader.readLine() ) != null )
+            {
+                String trimmedLn = ln.trim();
+                if ( !trimmedLn.isEmpty() )
+                {
+                    ret.add(ln.trim());
+                }
+            }
+
+            return ret.toArray( new String[ ret.size() ] );
+        }
+        catch ( IOException e )
+        {
+            throw new AnimationNotFound( name );
+        }
+    }
+
+    private SwingBitmap[] loadFrames(
+        SwingBitmapLoader bitmapLoader, String[] animation )
     {
         SwingBitmap[] ret = new SwingBitmap[animation.length];
         int i = 0;
@@ -174,9 +264,9 @@ public class AnimationTester extends JFrame
         }
 
         @Override
-        void draw( Graphics2D g )
+        void draw( java.awt.Graphics2D g )
         {
-            g.setPaint( Color.WHITE );
+            g.setPaint( java.awt.Color.WHITE );
             g.fillRect(
                 0,
                 0,
@@ -184,23 +274,38 @@ public class AnimationTester extends JFrame
                 canvas.getHeight()
             );
 
-            Sprite sprite0 = new Sprite(
-                frames[frameNum], scaler, 0, 1, 32, 0, 0 );
-            Sprite sprite1 = new Sprite(
-                frames[frameNum], scaler, 1, 1, 32, 0, 0 );
-            Sprite sprite2 = new Sprite(
-                frames[frameNum], scaler, 2, 1, 32, 0, 0 );
+            List<Sprite> sprites = new ArrayList<>();
+            int i = 0;
+            for ( SwingBitmap[] bmps : frames )
+            {
+                if ( bmps == null )
+                {
+                    ++i;
+                    continue;
+                }
+                java.awt.Point loc = int2dim( i );
+                Sprite sprite = new Sprite(
+                    bmps[frameNum], scaler, loc.x, loc.y, 32, 0, 0 );
+                sprites.add( sprite );
+                ++i;
+            }
 
             renderer.render(
                 new SwingCanvas( g ),
-                new Sprite[] {
-                        sprite0,
-                        sprite1,
-                        sprite2
-                },
+                    sprites.toArray( new Sprite[sprites.size() ] ),
                 paint
             );
         }
+    }
+
+    private java.awt.Point int2dim( int i )
+    {
+        return new java.awt.Point( i % 3, i / 3 );
+    }
+
+    private int screen2index( int x, int y )
+    {
+        return ( numTilesX * ( y / tileSize ) ) + ( x / tileSize );
     }
 
     private void pause()
