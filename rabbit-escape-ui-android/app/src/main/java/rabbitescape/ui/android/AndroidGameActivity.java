f906728c2f52832bb3c85f644cc370098a501215
Andy Balaam <andybalaam@artificialworlds.net>
2014-10-02 22:48:20 +0100

    Buttons bar on the left with some ability buttons populated from the world's list

--- rabbit-escape-ui-android/app/src/main/java/rabbitescape/ui/android/AndroidGameActivity.java
+++ rabbit-escape-ui-android/app/src/main/java/rabbitescape/ui/android/AndroidGameActivity.java
@@ -1,13 +1,27 @@
 package rabbitescape.ui.android;
 
 import android.content.res.Resources;
+import android.graphics.LightingColorFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.ToggleButton;
+
+import java.util.Map;
 
 import rabbitescape.engine.LoadWorldFile;
+import rabbitescape.engine.Token;
 import rabbitescape.engine.World;
 import rabbitescape.engine.util.RealFileSystem;
 import rabbitescape.render.BitmapCache;
@@ -19,13 +33,43 @@ public class AndroidGameActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_game );
-        RelativeLayout relativeLayout = (RelativeLayout)findViewById( R.id.relativeLayout );
+        LinearLayout topLayout = (LinearLayout)findViewById( R.id.topLayout );
 
         World world = new LoadWorldFile( new RealFileSystem() ).load( "test/level_01.rel" );
 
-        BitmapCache<AndroidBitmap> bitmapCache = createBitmapCache( this.getResources() );
+        Resources resources = getResources();
 
-        relativeLayout.addView( new MySurfaceView( this, bitmapCache, world ) );
+        createAbilityButtons( world, resources );
+
+        BitmapCache<AndroidBitmap> bitmapCache = createBitmapCache( resources );
+
+        topLayout.addView( new MySurfaceView( this, bitmapCache, world ) );
+    }
+
+    private void createAbilityButtons( World world, Resources resources )
+    {
+        RadioGroup abilitiesGroup = (RadioGroup)findViewById( R.id.abilitiesGroup );
+        for ( Token.Type ability : world.abilities.keySet() )
+        {
+            String name = ability.name();
+
+            ImageButton imgButton = new ImageButton( this );
+            imgButton.setImageDrawable(
+                resources.getDrawable(
+                    resources.getIdentifier(
+                        "ability_" + name,
+                        "drawable",
+                        "rabbitescape.ui.android"
+                    )
+                )
+            );
+
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT );
+            imgButton.setLayoutParams( params );
+            imgButton.setContentDescription( name );
+            abilitiesGroup.addView( imgButton );
+        }
     }
 
     private BitmapCache<AndroidBitmap> createBitmapCache( Resources resources )
