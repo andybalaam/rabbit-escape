9415cec3f66c99266c86fad912f3df7691f022db
Andy Balaam <andybalaam@artificialworlds.net>
2014-12-21 06:25:02 +0000

    New web address and version number in the about screen.

--- rabbit-escape-engine/src/rabbitescape/engine/menu/AboutText.java
+++ rabbit-escape-engine/src/rabbitescape/engine/menu/AboutText.java
@@ -2,13 +2,16 @@ package rabbitescape.engine.menu;
 
 public class AboutText
 {
+    public static final String version = "0.0.5";
+
     public static final String text =
           "Rabbit Escape\n"
         + "Copyright(c) 2014 by Andy Balaam\n"
+        + "Version " + version + "\n"
         + "\n"
         + "All code released under GPL v2.\n"
         + "All graphics, animations and levels released under CC-BY-NC.\n"
         + "See the file COPYING.txt for details.\n"
         + "\n"
-        + "https://github.com/andybalaam/rabbit-escape";
+        + "http://www.artificialworlds.net/rabbit-escape";
 }
