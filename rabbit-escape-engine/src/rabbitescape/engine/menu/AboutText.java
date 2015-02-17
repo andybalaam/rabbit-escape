a3a792660e30c90ef3e5ba0f39a226cfdccd98eb
Andy Balaam <andybalaam@artificialworlds.net>
2015-02-17 22:00:34 +0000

    Update code version number, split out URL in About text, and mention other contributors

--- rabbit-escape-engine/src/rabbitescape/engine/menu/AboutText.java
+++ rabbit-escape-engine/src/rabbitescape/engine/menu/AboutText.java
@@ -2,16 +2,20 @@ package rabbitescape.engine.menu;
 
 public class AboutText
 {
-    public static final String version = "0.0.5";
+    public static final String version = "0.1";
+
+    public static final String url =
+        "http://www.artificialworlds.net/rabbit-escape";
 
     public static final String text =
           "Rabbit Escape\n"
-        + "Copyright(c) 2014 by Andy Balaam\n"
+        + "Copyright(c) 2014-2015 by Andy Balaam and the "
+            + "Rabbit Escape developers\n"
         + "Version " + version + "\n"
         + "\n"
         + "All code released under GPL v2.\n"
         + "All graphics, animations and levels released under CC-BY-NC.\n"
         + "See the file COPYING.txt for details.\n"
         + "\n"
-        + "http://www.artificialworlds.net/rabbit-escape";
+        + url;
 }
