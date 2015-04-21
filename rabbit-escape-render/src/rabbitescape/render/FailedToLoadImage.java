800760435931d0d6b1718a75341da46f0915ecb7
Andy Balaam <andybalaam@artificialworlds.net>
2015-04-21 22:29:18 +0100

    Remember the cause of a failure to load an image

--- rabbit-escape-render/src/rabbitescape/render/FailedToLoadImage.java
+++ rabbit-escape-render/src/rabbitescape/render/FailedToLoadImage.java
@@ -6,15 +6,18 @@ public class FailedToLoadImage extends RabbitEscapeException
 {
     private static final long serialVersionUID = 1L;
     public final String imageName;
+    public final Throwable cause;
 
-    public FailedToLoadImage( String imageName, Exception cause )
+    public FailedToLoadImage( String imageName, Throwable cause )
     {
         super( cause );
         this.imageName = imageName;
+        this.cause = cause;
     }
 
     public FailedToLoadImage( String imageName )
     {
         this.imageName = imageName;
+        this.cause = null;
     }
 }
