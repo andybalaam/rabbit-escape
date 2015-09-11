d810d51ad4df11f970ac51596c1d7f3a7aaa8065
Andy Balaam <andybalaam@artificialworlds.net>
2015-09-11 02:33:56 +0100

    Enable the outdoors levels

--- rabbit-escape-engine/src/rabbitescape/engine/menu/MenuDefinition.java
+++ rabbit-escape-engine/src/rabbitescape/engine/menu/MenuDefinition.java
@@ -14,9 +14,10 @@ public class MenuDefinition
                 "Start Game",
                 menu(
                     "Choose a set of levels:",
-                    item( "Easy",   levels( "easy",   levelsCompleted ), true ),
-                    item( "Medium", levels( "medium", levelsCompleted ), true ),
-                    item( "Hard",   levels( "hard",   levelsCompleted ), true )
+                    item( "Easy",     levels( "easy",     levelsCompleted ), true ),
+                    item( "Medium",   levels( "medium",   levelsCompleted ), true ),
+                    item( "Hard",     levels( "hard",     levelsCompleted ), true ),
+                    item( "Outdoors", levels( "outdoors", levelsCompleted ), true )
                 ),
                 true
             ),
