8039d32e888c687c4c9334e26199ee285c7b3947
tttppp <tttppp@users.noreply.github.com>
2015-10-28 07:51:50 +0000

    Replace Collections with arrays.

--- rabbit-escape-engine/src/rabbitescape/engine/util/MegaCoder.java
+++ rabbit-escape-engine/src/rabbitescape/engine/util/MegaCoder.java
@@ -1,11 +1,8 @@
 package rabbitescape.engine.util;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.TreeSet;
 
 /**
  * Obfuscate strings like a champion.
@@ -24,7 +21,7 @@ import java.util.TreeSet;
 public class MegaCoder
 {
     /** A list of common ASCII characters that can be swapped with each other. */
-    private static final List<Character> COMMON_CHARACTERS = Arrays.asList(
+    private static final char[] COMMON_CHARACTERS = new char[] {
         ' ', '!', '"', '#', '$', '%', '&',
         '\'', '(', ')', '*', '+', ',',
         '-', '.', '/', '0', '1', '2', '3',
@@ -38,7 +35,7 @@ public class MegaCoder
         'd', 'e', 'f', 'g', 'h', 'i', 'j',
         'k', 'l', 'm', 'n', 'o', 'p', 'q',
         'r', 's', 't', 'u', 'v', 'w', 'x',
-        'y', 'z', '{', '|', '}', '~' );
+        'y', 'z', '{', '|', '}', '~' };
 
     /** Private constructor for util class. */
     private MegaCoder()
@@ -54,19 +51,17 @@ public class MegaCoder
      */
     public static String encode( String input )
     {
-        List<Character> characterList = Util.stringToCharacterList( input );
+        char[] charArray = input.toCharArray();
 
-        List<Character> sortedCharactersUsed = new ArrayList<>(
-            new TreeSet<Character>( characterList ) );
-        characterList = replaceUsingCharacterList( sortedCharactersUsed,
-            characterList );
+        char[] sortedCharsUsed = getSortedUniqueChars( charArray );
+        charArray = replaceUsingCharacterList( sortedCharsUsed, charArray );
 
-        characterList = replaceUsingCharacterList(
-            shuffle( COMMON_CHARACTERS, characterList.size() ), characterList );
+        charArray = replaceUsingCharacterList(
+            shuffle( COMMON_CHARACTERS, charArray.length ), charArray );
 
-        characterList = shuffle( characterList, 0 );
+        charArray = shuffle( charArray, 0 );
 
-        return Util.characterListToString( characterList );
+        return String.valueOf( charArray );
     }
 
     /**
@@ -78,41 +73,83 @@ public class MegaCoder
      */
     public static String decode( String input )
     {
-        List<Character> characterList = Util.stringToCharacterList( input );
+        char[] charArray = input.toCharArray();
 
-        characterList = shuffle( characterList, 0 );
+        charArray = shuffle( charArray, 0 );
 
-        characterList = replaceUsingCharacterList(
-            shuffle( COMMON_CHARACTERS, characterList.size() ), characterList );
+        charArray = replaceUsingCharacterList(
+            shuffle( COMMON_CHARACTERS, charArray.length ), charArray );
 
-        List<Character> sortedCharactersUsed = new ArrayList<>(
-            new TreeSet<Character>( characterList ) );
-        characterList = replaceUsingCharacterList( sortedCharactersUsed,
-            characterList );
+        char[] sortedCharsUsed = getSortedUniqueChars( charArray );
+        charArray = replaceUsingCharacterList( sortedCharsUsed, charArray );
 
-        return Util.characterListToString( characterList );
+        return String.valueOf( charArray );
+    }
+
+    /**
+     * Create a new character array containing every character that appears in a
+     * given array once. The new array will be sorted by natural order.
+     * 
+     * @param charArray
+     *            The character array to process.
+     * @return An array containing the sorted set of characters in the input.
+     */
+    private static char[] getSortedUniqueChars( char[] charArray )
+    {
+        // Create a sorted copy of the input array.
+        char[] sortedCharsUsed = Arrays.copyOf( charArray, charArray.length );
+        Arrays.sort( sortedCharsUsed );
+
+        // Handle the edge case of an empty array.
+        if ( sortedCharsUsed.length == 0 )
+        {
+            return sortedCharsUsed;
+        }
+
+        // Create a 'uniquified' copy of the array.
+        int uniqueCharCount = 1;
+        for ( int i = 0; i < sortedCharsUsed.length - 1; i++ )
+        {
+            if ( sortedCharsUsed[i] != sortedCharsUsed[i + 1] )
+            {
+                uniqueCharCount++;
+            }
+        }
+        char[] uniqueSortedCharsUsed = new char[uniqueCharCount];
+        uniqueSortedCharsUsed[0] = sortedCharsUsed[0];
+        int uniqueArrayIndex = 1;
+        for ( int i = 0; i < sortedCharsUsed.length - 1; i++ )
+        {
+            if ( sortedCharsUsed[i] != sortedCharsUsed[i + 1] )
+            {
+                uniqueSortedCharsUsed[uniqueArrayIndex] = sortedCharsUsed[i + 1];
+                uniqueArrayIndex++;
+            }
+        }
+
+        return uniqueSortedCharsUsed;
     }
 
     /**
      * Replace characters in a string using a key.
      * 
-     * @param characterListKey
-     *            A list of characters. The first will be swapped with the last,
-     *            and so on. If there is an odd number of characters then the
-     *            middle character will not be swapped at all.
+     * @param charsListKey
+     *            A list of distinct characters. The first will be swapped with
+     *            the last, and so on. If there is an odd number of characters
+     *            then the middle character will not be swapped at all.
      * @param input
      *            The string to replace the characters in.
      */
-    private static List<Character> replaceUsingCharacterList(
-        List<Character> characterListKey,
-        List<Character> input )
+    private static char[] replaceUsingCharacterList(
+        char[] charsListKey,
+        char[] input )
     {
-        List<Character> output = input;
-        int distinctCharacters = characterListKey.size();
+        char[] output = input;
+        int distinctCharacters = charsListKey.length;
         for ( int i = 0; i < distinctCharacters / 2; i++ )
         {
-            output = exchangeCharacters( output, characterListKey.get( i ),
-                characterListKey.get( distinctCharacters - i - 1 ) );
+            output = exchangeCharacters( output, charsListKey[i],
+                charsListKey[distinctCharacters - i - 1] );
         }
         return output;
     }
@@ -128,20 +165,20 @@ public class MegaCoder
      *            Another character.
      * @return The input, but with all occurrences of a and b exchanged.
      */
-    private static List<Character> exchangeCharacters(
-        List<Character> chars,
-        Character a,
-        Character b )
+    private static char[] exchangeCharacters(
+        char[] chars,
+        char a,
+        char b )
     {
-        for ( int i = 0; i < chars.size(); i++ )
+        for ( int i = 0; i < chars.length; i++ )
         {
-            if ( chars.get( i ).equals( a ) )
+            if ( chars[i] == a )
             {
-                chars.set( i, b );
+                chars[i] = b;
             }
-            else if ( chars.get( i ).equals( b ) )
+            else if ( chars[i] == b )
             {
-                chars.set( i, a );
+                chars[i] = a;
             }
         }
         return chars;
@@ -156,11 +193,11 @@ public class MegaCoder
      *            A value that changes the shuffling algorithm.
      * @return The string with letters swapped around a bit.
      */
-    private static List<Character> shuffle( List<Character> input, int key )
+    private static char[] shuffle( char[] input, int key )
     {
-        List<Character> output = new ArrayList<>( input );
+        char[] output = Arrays.copyOf( input, input.length );
         Set<Integer> swappedIndexes = new HashSet<>();
-        int length = output.size();
+        int length = output.length;
         for ( int i = 0; swappedIndexes.size() < ( length / 2 ) * 2; i++ )
         {
             if ( !swappedIndexes.contains( i ) )
@@ -171,9 +208,9 @@ public class MegaCoder
                     j = ( j + 1 ) % length;
                 }
 
-                Character temp = output.get( i );
-                output.set( i, output.get( j ) );
-                output.set( j, temp );
+                Character temp = output[i];
+                output[i] = output[j];
+                output[j] = temp;
 
                 swappedIndexes.add( i );
                 swappedIndexes.add( j );
