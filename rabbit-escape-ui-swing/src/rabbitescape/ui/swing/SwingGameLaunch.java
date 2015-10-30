6d4a98853d35cdbac0078634d7fe533a40a3404e
ColonelFazackerley <colonelfazackerley@users.noreply.github.com>
2015-10-30 19:31:05 +0000

    grab keyboard focus after dialog goes so keystrokes are not lost. fixes #102

--- rabbit-escape-ui-swing/src/rabbitescape/ui/swing/SwingGameLaunch.java
+++ rabbit-escape-ui-swing/src/rabbitescape/ui/swing/SwingGameLaunch.java
@@ -92,6 +92,9 @@ public class SwingGameLaunch implements GameLaunch
     public void run( String[] args )
     {
         showIntroDialog();
+        // After the dialog has gone, return keyboard 
+        // focus so keystrokes are not lost.
+        frame.getRootPane().grabFocus();
         loop.run();
     }
 
