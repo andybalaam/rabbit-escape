andybalaam/rabbit-escape

commit 1e7e87845818b60cd6b8b8aae20447a989025059

Make Instruction just a struct, and make performing instructions a separate process

rabbit-escape-engine/src/rabbitescape/engine/solution/SolutionRunner.java
@@ -1,6 +1,7 @@
 package rabbitescape.engine.solution;
 
 import rabbitescape.engine.World;
+import rabbitescape.engine.Token.Type;
 import rabbitescape.engine.World.DontStepAfterFinish;
 import rabbitescape.engine.World.NoSuchAbilityInThisWorld;
 import rabbitescape.engine.World.NoneOfThisAbilityLeft;
@@ -16,7 +17,7 @@ public class SolutionRunner
         {
             try
             {
-                instruction.performOn( sandboxGame );
+                performInstruction( instruction, sandboxGame );
             }
             catch ( DontStepAfterFinish e )
             {
@@ -43,4 +44,47 @@ public class SolutionRunner
         }
     }
 
+    private static void performInstruction(
+        Instruction instruction, final SandboxGame sandboxGame )
+    {
+        instruction.typeSwitch( new InstructionTypeSwitch()
+            {
+                @Override
+                public void caseWaitInstruction( WaitInstruction w )
+                {
+                    for ( int i = 0; i < w.steps; i++ )
+                    {
+                        sandboxGame.getWorld().step();
+                    }
+                }
+
+                @Override
+                public void caseSelectInstruction( SelectInstruction s )
+                {
+                    sandboxGame.setSelectedType( s.type );
+                }
+
+                @Override
+                public void caseTargetState( TargetState s )
+                {
+                    if (
+                        sandboxGame.getWorld().completionState()
+                            != s.targetState
+                    )
+                    {
+                        throw new InvalidSolution( "Solution " + s.solutionId
+                            + " did not cause " + s.targetState
+                            + " at instruction " + s.instructionIndex );
+                    }
+                }
+
+                @Override
+                public void casePlaceTokenInstruction( PlaceTokenInstruction p )
+                {
+                    Type type = sandboxGame.getSelectedType();
+                    sandboxGame.getWorld().changes.addToken( p.x, p.y, type );
+                }
+            }
+        );
+    }
 }

rabbit-escape-engine/src/rabbitescape/engine/solution/Instruction.java
@@ -2,5 +2,5 @@ package rabbitescape.engine.solution;
 
 public interface Instruction
 {
-    void performOn( SandboxGame sandboxGame );
+    void typeSwitch( InstructionTypeSwitch instructionTypeSwitch );
 }

rabbit-escape-engine/src/rabbitescape/engine/solution/InstructionTypeSwitch.java
@@ -1,0 +1,11 @@
+package rabbitescape.engine.solution;
+
+public interface InstructionTypeSwitch
+{
+    void caseWaitInstruction( WaitInstruction waitInstruction );
+    void caseSelectInstruction( SelectInstruction selectInstruction );
+    void caseTargetState( TargetState targetState );
+
+    void casePlaceTokenInstruction(
+        PlaceTokenInstruction placeTokenInstruction );
+}

rabbit-escape-engine/src/rabbitescape/engine/solution/PlaceTokenInstruction.java
@@ -1,7 +1,5 @@
 package rabbitescape.engine.solution;
 
-import rabbitescape.engine.Token.Type;
-
 public class PlaceTokenInstruction implements Instruction
 {
     int x;
@@ -21,13 +19,6 @@ public class PlaceTokenInstruction implements Instruction
     public int getY()
     {
         return y;
-    }
-
-    @Override
-    public void performOn( SandboxGame sandboxGame )
-    {
-        Type type = sandboxGame.getSelectedType();
-        sandboxGame.getWorld().changes.addToken( x, y, type );
     }
 
     @Override
@@ -52,5 +43,11 @@ public class PlaceTokenInstruction implements Instruction
     public int hashCode()
     {
         return x + 31 * ( x + y );
+    }
+
+    @Override
+    public void typeSwitch( InstructionTypeSwitch instructionTypeSwitch )
+    {
+        instructionTypeSwitch.casePlaceTokenInstruction( this );
     }
 }

rabbit-escape-engine/src/rabbitescape/engine/solution/SelectInstruction.java
@@ -4,17 +4,11 @@ import rabbitescape.engine.Token;
 
 public class SelectInstruction implements Instruction
 {
-    private final Token.Type type;
+    public final Token.Type type;
 
     public SelectInstruction( Token.Type type )
     {
         this.type = type;
-    }
-
-    @Override
-    public void performOn( SandboxGame sandboxGame )
-    {
-        sandboxGame.setSelectedType( type );
     }
 
     @Override
@@ -39,5 +33,11 @@ public class SelectInstruction implements Instruction
     public int hashCode()
     {
         return type.hashCode();
+    }
+
+    @Override
+    public void typeSwitch( InstructionTypeSwitch instructionTypeSwitch )
+    {
+        instructionTypeSwitch.caseSelectInstruction( this );
     }
 }

rabbit-escape-engine/src/rabbitescape/engine/solution/TargetState.java
@@ -4,9 +4,9 @@ import rabbitescape.engine.World.CompletionState;
 
 public class TargetState implements ValidationInstruction
 {
-    private final CompletionState targetState;
-    private final int solutionId;
-    private final int instructionIndex;
+    public final CompletionState targetState;
+    public final int solutionId;
+    public final int instructionIndex;
 
     public TargetState(
         CompletionState targetState,
@@ -16,17 +16,6 @@ public class TargetState implements ValidationInstruction
         this.targetState = targetState;
         this.solutionId = solutionId;
         this.instructionIndex = instructionIndex;
-    }
-
-    @Override
-    public void performOn( SandboxGame sandboxGame )
-    {
-        if ( sandboxGame.getWorld().completionState() != targetState )
-        {
-            throw new InvalidSolution( "Solution " + solutionId
-                + " did not cause " + targetState
-                + " at instruction " + instructionIndex );
-        }
     }
 
     @Override
@@ -64,5 +53,11 @@ public class TargetState implements ValidationInstruction
         result = prime * result + solutionId;
         result = prime * result + targetState.hashCode();
         return result;
+    }
+
+    @Override
+    public void typeSwitch( InstructionTypeSwitch instructionTypeSwitch )
+    {
+        instructionTypeSwitch.caseTargetState( this );
     }
 }

rabbit-escape-engine/src/rabbitescape/engine/solution/WaitInstruction.java
@@ -2,20 +2,11 @@ package rabbitescape.engine.solution;
 
 public class WaitInstruction implements Instruction
 {
-    private final int steps;
+    public final int steps;
 
     public WaitInstruction( int steps )
     {
         this.steps = steps;
-    }
-
-    @Override
-    public void performOn( SandboxGame sandboxGame )
-    {
-        for ( int i = 0; i < steps; i++ )
-        {
-            sandboxGame.getWorld().step();
-        }
     }
 
     @Override
@@ -40,5 +31,11 @@ public class WaitInstruction implements Instruction
     public int hashCode()
     {
         return steps;
+    }
+
+    @Override
+    public void typeSwitch( InstructionTypeSwitch instructionTypeSwitch )
+    {
+        instructionTypeSwitch.caseWaitInstruction( this );
     }
 }

commit 8382a3f347f9639eefd645e386fb4197ce184c34

Fix compile error in InputHandler introduced in 1e7e87845818b60cd6b8b8aae20447a989025059 (sorry)

rabbit-escape-engine/src/rabbitescape/engine/solution/SolutionRunner.java
@@ -44,7 +44,7 @@ public class SolutionRunner
         }
     }
 
-    private static void performInstruction(
+    public static void performInstruction(
         Instruction instruction, final SandboxGame sandboxGame )
     {
         instruction.typeSwitch( new InstructionTypeSwitch()

rabbit-escape-ui-text/src/rabbitescape/ui/text/InputHandler.java
@@ -10,6 +10,7 @@ import rabbitescape.engine.err.RabbitEscapeException;
 import rabbitescape.engine.solution.Instruction;
 import rabbitescape.engine.solution.SandboxGame;
 import rabbitescape.engine.solution.SolutionFactory;
+import rabbitescape.engine.solution.SolutionRunner;
 
 public class InputHandler
 {
@@ -141,7 +142,7 @@ public class InputHandler
 
             for ( Instruction instr : instructions )
             {
-                instr.performOn( sandboxGame );
+                SolutionRunner.performInstruction( instr, sandboxGame );
             }
 
             append( input );