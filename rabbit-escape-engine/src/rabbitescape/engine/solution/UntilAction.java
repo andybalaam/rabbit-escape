51f0f4e36c03f4cc0366b784468fa003cfb3a833
Andy Balaam <andybalaam@artificialworlds.net>
2015-11-14 23:58:48 +0000

    Add file missed from previous commit.

---
+++ rabbit-escape-engine/src/rabbitescape/engine/solution/UntilAction.java
@@ -0,0 +1,53 @@
+package rabbitescape.engine.solution;
+
+import rabbitescape.engine.World.CompletionState;
+
+public class UntilAction implements SolutionAction
+{
+    public final CompletionState targetState;
+
+    public UntilAction( CompletionState targetState )
+    {
+        this.targetState = targetState;
+    }
+
+    @Override
+    public String relFormat( boolean firstInCommand )
+    {
+        if ( firstInCommand )
+        {
+            targetState.name();
+        }
+        return SolutionParser.ACTION_DELIMITER + targetState.name();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "UntilAction( " + targetState.name() + " )";
+    }
+
+    @Override
+    public boolean equals( Object otherObj )
+    {
+        if ( ! ( otherObj instanceof UntilAction ) )
+        {
+            return false;
+        }
+        UntilAction other = (UntilAction)otherObj;
+
+        return ( targetState == other.targetState );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return targetState.hashCode();
+    }
+
+    @Override
+    public void typeSwitch( ActionTypeSwitch actionTypeSwitch )
+    {
+        actionTypeSwitch.caseAssertStateAction( this );
+    }
+}
