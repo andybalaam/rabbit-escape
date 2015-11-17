f0e1193c290f374ec61799438131570d7d2f3907
ColonelFazackerley <colonelfazackerley@users.noreply.github.com>
2015-11-17 12:10:04 +0000

    remove non-ASCII char from test

--- rabbit-escape-engine/test/rabbitescape/engine/TestUtil.java
+++ rabbit-escape-engine/test/rabbitescape/engine/TestUtil.java
@@ -955,13 +955,13 @@ public class TestUtil
         s = "I've seen things you people wouldn't believe. " + 
             "Attack ships on fire off the shoulder of Orion. "+
             "I watched C-beams glitter in the dark near the " +
-            "Tannhäuser Gate. All those moments will be lost " +
+            "Tannhauser Gate. All those moments will be lost " +
             "in time, like tears...in...rain. Time to die";
         l = new String[] {
             "I've seen things you people wouldn't believe.",
             "Attack ships on fire off the shoulder of Orion.",
             "I watched C-beams glitter in the dark near the",
-            "Tannhäuser Gate. All those moments will be lost",
+            "Tannhauser Gate. All those moments will be lost",
             "in time, like tears...in...rain. Time to die"
         };
           assertThat( Util.wrap( s, 47 ), equalTo(l));
