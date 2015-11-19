beae96a26e41d60817dccbef06e43919247a8f9b
ColonelFazackerley <colonelfazackerley@users.noreply.github.com>
2015-11-19 19:29:25 +0000

    start implementing until:WON for solutions

--- rabbit-escape-engine/src/rabbitescape/engine/solution/SolutionParser.java
+++ rabbit-escape-engine/src/rabbitescape/engine/solution/SolutionParser.java
@@ -15,6 +15,7 @@ public class SolutionParser
     public static final String COMMAND_DELIMITER = ";";
     public static final String ACTION_DELIMITER = "&";
     private static final Pattern WAIT_REGEX = Pattern.compile( "\\d+" );
+    private static final Pattern UNTIL_REGEX = Pattern.compile( "until:([A-Z]+)" );
 
     private static final Pattern PLACE_TOKEN_REGEX = Pattern.compile(
         "\\((\\d+),(\\d+)\\)" );
@@ -73,10 +74,15 @@ public class SolutionParser
     private static SolutionAction doMakeAction( String actionString )
     throws NumberFormatException, InvalidAction
     {
+        Matcher untilMatcher = UNTIL_REGEX.matcher( actionString );
         if ( COMPLETION_STATES.contains( actionString ) )
         {
             return new AssertStateAction(
                 CompletionState.valueOf( actionString ) );
+        }
+        else if ( untilMatcher.matches() ) {
+            CompletionState state = CompletionState.valueOf( untilMatcher.group( 1 ) );
+            return new UntilAction(state);
         }
         else if ( WAIT_REGEX.matcher( actionString ).matches() )
         {
