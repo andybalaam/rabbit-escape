abe7a8bfeba767a72ab2f2dc0137f0f1a1af7215
ColonelFazackerley <colonelfazackerley@users.noreply.github.com>
2015-11-20 22:38:05 +0000

    small class to represent token selection shortcuts

---
+++ rabbit-escape-engine/src/rabbitescape/engine/textworld/InputExpansion.java
@@ -0,0 +1,29 @@
+package rabbitescape.engine.textworld;
+
+public class InputExpansion
+{
+
+    public final String character, expansion;
+    
+    public final static InputExpansion[] expansions = new InputExpansion[] {
+        new InputExpansion( "b", "bash" ),
+        new InputExpansion( "d", "dig" ),
+        new InputExpansion( "i", "bridge" ),
+        new InputExpansion( "k", "block" ),
+        new InputExpansion( "c", "climb" ),
+        new InputExpansion( "p", "explode" )
+    };
+    
+    public InputExpansion( String character, String expansion )
+    {
+        this.character = character;
+        this.expansion = expansion;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return character + ": " + expansion ;
+    }
+
+}
