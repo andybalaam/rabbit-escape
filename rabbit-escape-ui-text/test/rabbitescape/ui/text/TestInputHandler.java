ddde558f18a2d056825e1c9cfe2b5a8cbd024f39
ColonelFazackerley <colonelfazackerley@users.noreply.github.com>
2015-11-25 22:34:24 +0000

    add to expandAbbreviations test

--- rabbit-escape-ui-text/test/rabbitescape/ui/text/TestInputHandler.java
+++ rabbit-escape-ui-text/test/rabbitescape/ui/text/TestInputHandler.java
@@ -11,11 +11,21 @@ public class TestInputHandler
     public void Expand_abbreviations()
     {
         String abbrev, expected, expanded;
+
         abbrev = "";
         expected = "1" ;
         expanded = InputHandler.expandAbbreviations( abbrev );
         assertThat( expanded, equalTo( expected ) );
         
+        abbrev = "i;b;d;k;1,2;c;p";
+        expected = "bridge;bash;dig;block;(1,2);climb;explode" ;
+        expanded = InputHandler.expandAbbreviations( abbrev );
+        assertThat( expanded, equalTo( expected ) );
+        
+        abbrev = "in;bridge;di;ok;(1,2)";
+        expected = "in;bridge;di;ok;(1,2)" ;
+        expanded = InputHandler.expandAbbreviations( abbrev );
+        assertThat( expanded, equalTo( expected ) );
     }
     
 }
