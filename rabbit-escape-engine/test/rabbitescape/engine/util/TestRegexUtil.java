695dbf7a6a7689a84798098d25bb042fcc3b1e66
ColonelFazackerley <colonelfazackerley@users.noreply.github.com>
2015-11-25 07:49:16 +0000

    tests for regex methods in Util

---
+++ rabbit-escape-engine/test/rabbitescape/engine/util/TestRegexUtil.java
@@ -0,0 +1,39 @@
+package rabbitescape.engine.util;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class TestRegexUtil
+{
+
+    @Test
+    public void Regex_remove_preserve_group()
+    {
+        String before, regex, after;
+        
+        before = "Level: \\\"Tetris\\\"";
+        regex  = "\\\\(\")";
+        after  = "Level: \"Tetris\"";
+        assertThat(Util.regexRemovePreserveGroup( before , regex ), equalTo(after));
+        
+        before = "\\\"Thing in escaped quotes\\\" \"Thing in quotes\"";
+        regex  = "\\\\(\")";
+        after  = "\"Thing in escaped quotes\" \"Thing in quotes\"";
+        assertThat(Util.regexRemovePreserveGroup( before , regex ), equalTo(after));
+    }
+    
+    @Test
+    public void Regex_replace()
+    {
+        String before, regex, replacement, after;
+        
+        before = "The brown fox.";
+        regex  = "brown";
+        replacement = "red";
+        after  = "The red fox.";
+        assertThat(Util.regexReplace( before, regex, replacement ), equalTo(after));
+    }
+    
+}
