444a5df3c07eeaf7b167bd89f7ad1f9318dee834
ColonelFazackerley <colonelfazackerley@users.noreply.github.com>
2015-12-15 18:08:20 +0000

    update falling test for new states: used to decide if tokens consumed

--- rabbit-escape-engine/src/rabbitescape/engine/BehaviourTools.java
+++ rabbit-escape-engine/src/rabbitescape/engine/BehaviourTools.java
@@ -36,21 +36,33 @@ public class BehaviourTools
 
     public boolean rabbitIsFalling()
     {
-        return
-               State.RABBIT_FALLING == rabbit.state
-            || State.RABBIT_FALLING_1 == rabbit.state
-            || State.RABBIT_FALLING_1_TO_DEATH == rabbit.state
-            || State.RABBIT_DYING_OF_FALLING_2 == rabbit.state
-            || State.RABBIT_DYING_OF_FALLING == rabbit.state
-            || State.RABBIT_FALLING_ONTO_LOWER_RIGHT == rabbit.state
-            || State.RABBIT_FALLING_ONTO_RISE_RIGHT == rabbit.state
-            || State.RABBIT_FALLING_ONTO_LOWER_LEFT == rabbit.state
-            || State.RABBIT_FALLING_ONTO_RISE_LEFT == rabbit.state
-            || State.RABBIT_FALLING_1_ONTO_LOWER_RIGHT == rabbit.state
-            || State.RABBIT_FALLING_1_ONTO_RISE_RIGHT == rabbit.state
-            || State.RABBIT_FALLING_1_ONTO_LOWER_LEFT == rabbit.state
-            || State.RABBIT_FALLING_1_ONTO_RISE_LEFT == rabbit.state 
-            || State.RABBIT_DYING_OF_FALLING_SLOPE_RISE_RIGHT == rabbit.state;
+        switch (rabbit.state)
+        {
+        case RABBIT_FALLING:
+        case RABBIT_FALLING_1:
+        case RABBIT_FALLING_1_TO_DEATH:
+        case RABBIT_DYING_OF_FALLING_2:
+        case RABBIT_DYING_OF_FALLING:
+        case RABBIT_FALLING_ONTO_LOWER_RIGHT:
+        case RABBIT_FALLING_ONTO_RISE_RIGHT:
+        case RABBIT_FALLING_ONTO_LOWER_LEFT:
+        case RABBIT_FALLING_ONTO_RISE_LEFT:
+        case RABBIT_FALLING_1_ONTO_LOWER_RIGHT:
+        case RABBIT_FALLING_1_ONTO_RISE_RIGHT:
+        case RABBIT_FALLING_1_ONTO_LOWER_LEFT:
+        case RABBIT_FALLING_1_ONTO_RISE_LEFT:
+        case RABBIT_DYING_OF_FALLING_SLOPE_RISE_RIGHT:
+        case RABBIT_DYING_OF_FALLING_SLOPE_RISE_RIGHT_2:
+        case RABBIT_DYING_OF_FALLING_2_SLOPE_RISE_RIGHT:
+        case RABBIT_DYING_OF_FALLING_2_SLOPE_RISE_RIGHT_2:
+        case RABBIT_DYING_OF_FALLING_SLOPE_RISE_LEFT:
+        case RABBIT_DYING_OF_FALLING_SLOPE_RISE_LEFT_2:
+        case RABBIT_DYING_OF_FALLING_2_SLOPE_RISE_LEFT:
+        case RABBIT_DYING_OF_FALLING_2_SLOPE_RISE_LEFT_2:
+            return true;
+        default:
+            return false;
+        }
     }
     
     public boolean pickUpToken( Token.Type type, boolean evenIfNotOnGround )
