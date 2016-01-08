50744fd371db8719b69e371ef4c4c8d65e381ead
ColonelFazackerley <colonelfazackerley@users.noreply.github.com>
2016-01-08 20:47:46 +0000

    use switch of states instead of rabbit.state.toString for climbing test

--- rabbit-escape-engine/src/rabbitescape/engine/BehaviourTools.java
+++ rabbit-escape-engine/src/rabbitescape/engine/BehaviourTools.java
@@ -65,7 +65,24 @@ public class BehaviourTools
 
     public boolean rabbitIsClimbing()
     {
-        return rabbit.state.toString().contains( "CLIMB" );
+        switch( rabbit.state)
+        {
+        case RABBIT_ENTERING_EXIT_CLIMBING_RIGHT:
+        case RABBIT_ENTERING_EXIT_CLIMBING_LEFT:
+        case RABBIT_CLIMBING_LEFT_START:
+        case RABBIT_CLIMBING_LEFT_CONTINUE_1:
+        case RABBIT_CLIMBING_LEFT_CONTINUE_2:
+        case RABBIT_CLIMBING_LEFT_END:
+        case RABBIT_CLIMBING_LEFT_BANG_HEAD:
+        case RABBIT_CLIMBING_RIGHT_START:
+        case RABBIT_CLIMBING_RIGHT_CONTINUE_1:
+        case RABBIT_CLIMBING_RIGHT_CONTINUE_2:
+        case RABBIT_CLIMBING_RIGHT_END:
+        case RABBIT_CLIMBING_RIGHT_BANG_HEAD:
+            return true;
+        default:
+            return false;
+        }
     }
     
     /**
