c8898dfa3aaaeec9dc500c6b9c4cd3c4e40333e4
tttppp <tttppp@users.noreply.github.com>
2016-02-21 08:41:34 +0000

    Refactor Direction as an enum.

--- rabbit-escape-engine/src/rabbitescape/engine/Direction.java
+++ rabbit-escape-engine/src/rabbitescape/engine/Direction.java
@@ -1,34 +1,18 @@
 package rabbitescape.engine;
 
-
-public class Direction
+public enum Direction
 {
-    private enum D
-    {
-        UP, RIGHT, DOWN, LEFT
-    }
-
-    public static final Direction UP    = new Direction( D.UP );
-    public static final Direction RIGHT = new Direction( D.RIGHT );
-    public static final Direction DOWN  = new Direction( D.DOWN );
-    public static final Direction LEFT  = new Direction( D.LEFT );
-
-    private final D dir;
-
-    public Direction( D dir )
-    {
-        this.dir = dir;
-    }
+    UP, RIGHT, DOWN, LEFT;
 
     public static Direction opposite( Direction input )
     {
-        switch( input.dir )
+        switch( input )
         {
             case UP:    return DOWN;
             case RIGHT: return LEFT;
             case DOWN:  return UP;
             case LEFT:  return RIGHT;
-            default: throw new IllegalArgumentException( input.dir.name() );
+            default: throw new IllegalArgumentException( input.name() );
         }
     }
 }
