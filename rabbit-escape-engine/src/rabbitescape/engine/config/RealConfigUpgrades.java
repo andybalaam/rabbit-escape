86e60b5fd0ef1d3b2230c5ebc7ae8d5cdfec9dd2
Andy Balaam <andybalaam@artificialworlds.net>
2016-03-04 21:29:24 +0000

    RealConfigUpgrades: the definitive list of upgraders #258

---
+++ rabbit-escape-engine/src/rabbitescape/engine/config/RealConfigUpgrades.java
@@ -0,0 +1,39 @@
+package rabbitescape.engine.config;
+
+import rabbitescape.engine.config.upgrades.UpgradeTo01NonAndroidDummy;
+
+public class RealConfigUpgrades
+{
+    /**
+     * Create the standard list of upgrades for non-Android versions,
+     * including a dummy empty upgrade from version 0 to 1.
+     *
+     * @return the list of currently-enabled config upgrades.
+     *         The first upgrades from version 0 to 1,
+     *         the second upgrades from version 1 to 2, etc.
+     */
+    public static IConfigUpgrade[] realConfigUpgrades()
+    {
+        return realConfigUpgrades( new UpgradeTo01NonAndroidDummy() );
+    }
+
+    /**
+     * Create the standard list of upgrades, allowing you to supply
+     * the first upgrade to run.  This is used by the Android code to
+     * supply the Android version of the 0->1 upgrade, because it is different.
+     *
+     * @param the first upgrade to be put in the list
+     *
+     * @return the list of currently-enabled config upgrades.
+     *         The first upgrades from version 0 to 1,
+     *         the second upgrades from version 1 to 2, etc.
+     */
+    public static IConfigUpgrade[] realConfigUpgrades(
+        IConfigUpgrade upgradeTo1 )
+    {
+        return new IConfigUpgrade[]
+        {
+            upgradeTo1
+        };
+    }
+}
