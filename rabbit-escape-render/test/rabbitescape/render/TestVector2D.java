ddb256c2c4718cbdd6d18787b2c0e2b3515128a4
colonelfazackerley <colonelfazackerley@users.noreply.github.com>
2016-04-04 21:12:59 +0100

    no need to extend BaseMatcher in TestVector2D

--- rabbit-escape-render/test/rabbitescape/render/TestVector2D.java
+++ rabbit-escape-render/test/rabbitescape/render/TestVector2D.java
@@ -3,8 +3,6 @@ package rabbitescape.render;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
-import org.hamcrest.BaseMatcher;
-import org.hamcrest.Description;
 import org.junit.Test;
 
 import rabbitescape.engine.CellularDirection;
@@ -16,7 +14,7 @@ public class TestVector2D
     {
         Vector2D a = new Vector2D( 1, 2 );
         Vector2D b = new Vector2D( 3, 4 );
-        assertThat( a.add( b ), equalToVector2D( new Vector2D( 4, 6 ) ) );
+        assertThat( a.add( b ), equalTo( new Vector2D( 4, 6 ) ) );
     }
 
     @Test
@@ -24,35 +22,35 @@ public class TestVector2D
     {
         Vector2D a = new Vector2D( 1, 2 );
         Vector2D b = new Vector2D( 3, 4 );
-        assertThat( a.subtract( b ), equalToVector2D( new Vector2D( -2, -2 ) ) );
+        assertThat( a.subtract( b ), equalTo( new Vector2D( -2, -2 ) ) );
     }
 
     @Test
     public void Vectors_multiply_with_double()
     {
         Vector2D a = new Vector2D( 4, 6 );
-        assertThat( a.multiply( 1.5 ), equalToVector2D( new Vector2D( 6, 9 ) ) );
+        assertThat( a.multiply( 1.5 ), equalTo( new Vector2D( 6, 9 ) ) );
     }
 
     @Test
     public void Vectors_multiply_with_int()
     {
         Vector2D a = new Vector2D( 4, 6 );
-        assertThat( a.multiply( 2 ), equalToVector2D( new Vector2D( 8, 12 ) ) );
+        assertThat( a.multiply( 2 ), equalTo( new Vector2D( 8, 12 ) ) );
     }
 
     @Test
     public void Vectors_can_be_divided()
     {
         Vector2D a = new Vector2D( 4, 6 );
-        assertThat( a.divide( 2 ), equalToVector2D( new Vector2D( 2, 3 ) ) );
+        assertThat( a.divide( 2 ), equalTo( new Vector2D( 2, 3 ) ) );
     }
 
     @Test
     public void Vectors_can_be_created_from_cellular_directions()
     {
         Vector2D a = Vector2D.translate( CellularDirection.DOWN );
-        assertThat( a, equalToVector2D( new Vector2D( 0, 1 ) ) );
+        assertThat( a, equalTo( new Vector2D( 0, 1 ) ) );
     }
 
     @Test
@@ -93,33 +91,4 @@ public class TestVector2D
         assertThat( a.toString(), equalTo( "(0003,0004)" ) );
     }
 
-    // ----------------
-
-    public Vector2DMatcher equalToVector2D( Vector2D v )
-    {
-        return new Vector2DMatcher( v );
-    }
-
-    private class Vector2DMatcher extends BaseMatcher<Vector2D>
-    {
-
-        Vector2D v;
-
-        public Vector2DMatcher( Vector2D v )
-        {
-            this.v = v;
-        }
-
-        @Override
-        public boolean matches( Object o )
-        {
-            return v.equals( (Vector2D)o );
-        }
-
-        @Override
-        public void describeTo( Description d )
-        {
-            d.appendText( v.toString() );
-        }
-    }
 }
