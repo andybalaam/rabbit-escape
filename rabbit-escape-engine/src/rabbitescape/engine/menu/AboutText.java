508fa11d922c0a299d20d3d2822d98122f4e3251
Andy Balaam <andybalaam@artificialworlds.net>
2017-01-11 02:24:12 +0000

    Change the license to pure GPL v2 or later.  Previously, levels,
    graphics and sound effects were under the non-free CC-BY-SA-NC license.

--- rabbit-escape-engine/src/rabbitescape/engine/menu/AboutText.java
+++ rabbit-escape-engine/src/rabbitescape/engine/menu/AboutText.java
@@ -9,12 +9,11 @@ public class AboutText
 
     public static final String text =
           "Rabbit Escape\n"
-        + "Copyright(c) 2014-2016 by Andy Balaam and the "
+        + "Copyright(c) 2014-2017 by Andy Balaam and the "
             + "Rabbit Escape developers\n"
         + "Version " + version + "\n"
         + "\n"
-        + "All code released under GPL v2.\n"
-        + "All graphics, animations and levels released under CC BY-NC-SA 3.0.\n"
+        + "Released under GPL v2.\n"
         + "Music is by tryad (http://tryad.org), used under CC BY-SA 3.0.\n"
         + "See the file COPYING.txt for details.\n"
         + "\n"
