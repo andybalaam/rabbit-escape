a797e7f9bba2c54ee5c95427edd8315cccda9c6d
Andy Balaam <andybalaam@artificialworlds.net>
2017-01-11 02:37:14 +0000

    Minor copyright statement updates

--- rabbit-escape-engine/src/rabbitescape/engine/menu/AboutText.java
+++ rabbit-escape-engine/src/rabbitescape/engine/menu/AboutText.java
@@ -8,12 +8,11 @@ public class AboutText
         "http://www.artificialworlds.net/rabbit-escape";
 
     public static final String text =
-          "Rabbit Escape\n"
+          "Rabbit Escape (version " + version + ")\n"
         + "Copyright(c) 2014-2017 by Andy Balaam and the "
             + "Rabbit Escape developers\n"
-        + "Version " + version + "\n"
-        + "\n"
         + "Released under GPL v2.\n"
+        + "\n"
         + "Music is by tryad (http://tryad.org), used under CC BY-SA 3.0.\n"
         + "See the file COPYING.txt for details.\n"
         + "\n"
