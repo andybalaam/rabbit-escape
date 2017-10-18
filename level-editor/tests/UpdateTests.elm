ef3bc07c2262b56d990853c3941430ef2511815a
Andy Balaam <andybalaam@artificialworlds.net>
2017-10-18 17:50:14 +0100

    Remodel update tests

--- level-editor/tests/UpdateTests.elm
+++ level-editor/tests/UpdateTests.elm
@@ -14,141 +14,150 @@ import WorldParser exposing (parse, parseErrToString)
 all : Test
 all =
     describe "Tests of the update function"
-        [ test "Clicking empty square adds block" clickEmptySquareAddsBlock
-        , test "Clicking full replaces with chosen block" clickFullReplaces
-        , test "ChangeMode changes mode" changeModeChangesMode
-        , test "Changing to code mode stores code text" codeModeStoresText
-        , test "Choosing a block updates block and mode" choosingBlockUpdates
+        [ testActions "Clicking empty square adds block"
+            ( [ "####"
+              , "#  #"
+              , "# r#"
+              , "####"
+              ]
+            , { mode = InitialMode, block = Nothing }
+            )
+            [ ( (LevelClick 2 1)
+              , [ "####"
+                , "# ##"
+                , "# r#"
+                , "####"
+                ]
+              , { mode = InitialMode, block = Nothing }
+              )
+            ]
+
+        , testActions "Clicking full replaces with chosen block"
+            ( [ "####"
+              , "#  #"
+              , "# r#"
+              , "####"
+              ]
+            , { mode = PlaceBlockMode
+              , block = Just (Block Earth UpRight)
+              }
+            )
+            [ ( (LevelClick 0 1)
+              , [ "####"
+                , "/  #"
+                , "# r#"
+                , "####"
+                ]
+              , { mode = PlaceBlockMode
+                , block = Just (Block Earth UpRight)
+                }
+              )
+            ]
+
+        , testActions "ChangeMode changes mode"
+            ( [ "" ]
+            , { mode = InitialMode, block = Nothing }
+            )
+            [ ( (ChangeMode ChooseBlockMode)
+              , [ "" ]
+              , { mode = ChooseBlockMode, block = Nothing }
+              )
+            ]
+
+        , testActions "Changing to code mode stores code text"
+            ( [ "#r#" ]
+            , { mode = InitialMode, block = Nothing }
+            )
+            [ ( (ChangeMode (CodeMode ""))  -- Even though we sent nothing
+              , [ "#r#" ]
+              , { mode = CodeMode "#r#", block = Nothing }
+                -- We get a model
+              )
+            ]
+
+        , testActions "Choosing a block updates block and mode"
+            ( [ "" ]
+            , { mode = InitialMode, block = Nothing }
+            )
+            [ ( (ChangeBlock (Block Earth UpRight))
+              , [ "" ]
+              , { mode = PlaceBlockMode
+                , block = Just (Block Earth UpRight)
+                }
+              )
+            ]
+
+        , testActions "Can undo an action"
+            (
+                [ "   "
+                , "   "
+                ]
+              , { mode = InitialMode, block = Nothing }
+            )
+            [ ( (LevelClick 2 0)
+              , [ "  #"
+                , "   "
+                ]
+              , { mode = InitialMode, block = Nothing }
+              )
+            --, ( Undo
+            --  , [ "   "
+            --    , "   "
+            --    ]
+            --  , { mode = InitialMode, block = Nothing }
+            --  )
+            ]
         ]
 
 
-parseFixed : String -> World
-parseFixed textWorld =
-    case parse "" textWorld of
+parseFixed : List String -> World
+parseFixed textLines =
+    case parse "" (String.join "\n" textLines) of
         Err e -> Debug.crash (parseErrToString e)
         Ok w -> w
 
 
-emptyModel : Model
-emptyModel =
-    { world = parseFixed ""
-    , uiState = { mode = InitialMode, block = Nothing }
-    , t = \x -> x
-    }
-
-
-smallModel =
-    { world = parseFixed "#r#"
-    , uiState = { mode = InitialMode, block = Nothing }
-    , t = \x -> x
-    }
-
-
-clickEmptySquareAddsBlock : () -> Expect.Expectation
-clickEmptySquareAddsBlock =
-    \() ->
-        Expect.equal
-            (
-                { emptyModel
-                | world = parseFixed "####\n# ##\n# r#\n####"
-                }
-            , Cmd.none
-            )
-            ( update
-                (LevelClick 2 1)
-                (
-                    { emptyModel
-                    | world = parseFixed "####\n#  #\n# r#\n####"
-                    }
-                )
-            )
-
-
-clickFullReplaces : () -> Expect.Expectation
-clickFullReplaces =
-    \() ->
-        Expect.equal
-            (
-                { emptyModel
-                | world = parseFixed "####\n/  #\n# r#\n####"
-                , uiState =
-                    { mode = PlaceBlockMode
-                    , block = Just (Block Earth UpRight)
-                    }
-                }
-            , Cmd.none
-            )
-            ( update
-                (LevelClick 0 1)
-                (
-                    { emptyModel
-                    | world = parseFixed "####\n#  #\n# r#\n####"
-                    , uiState =
-                        { mode = PlaceBlockMode
-                        , block = Just (Block Earth UpRight)
-                        }
-                    }
-                )
-            )
-
-
-changeModeChangesMode : () -> Expect.Expectation
-changeModeChangesMode =
+testActions :
+    String ->
+    ( List String, UiState ) ->
+    List (Msg, List String, UiState) ->
+    Test
+testActions desc (initWorld, initState) msgsAndWorlds =
     let
-       uiState = emptyModel.uiState
+        t = \x -> x
+
+        parseWorld : (Msg, List String, UiState) -> (Msg, Model)
+        parseWorld (msg, lines, state) =
+            (msg, {world = parseFixed lines, uiState = state, t = t})
+
+        initModel : Model
+        initModel =
+            { world = parseFixed initWorld
+            , uiState = initState
+            , t = t
+            }
+
+        msgsAndModels : List (Msg, Model)
+        msgsAndModels =
+            List.map parseWorld msgsAndWorlds
+
     in
-        \() ->
-            Expect.equal
-                (
-                    { emptyModel
-                    | uiState = { uiState | mode = ChooseBlockMode }
-                    }
-                , Cmd.none
-                )
-                ( update
-                    (ChangeMode ChooseBlockMode)
-                    emptyModel
-                )
+        describe desc (expectUpdateGives 1 initModel msgsAndModels)
 
 
-codeModeStoresText : () -> Expect.Expectation
-codeModeStoresText =
+expectUpdateGives : Int -> Model -> List (Msg, Model) -> List (Test)
+expectUpdateGives num initModel msgsAndModels =
     let
-       uiState = smallModel.uiState
+        t : Model -> Msg -> Model -> Test
+        t before msg after =
+            test
+                ( "Step " ++ (toString num) )
+                ( \() ->
+                    Expect.equal ( after, Cmd.none ) ( update msg before )
+                )
     in
-        \() ->
-            Expect.equal
-                (
-                    { smallModel
-                    | uiState = { uiState | mode = CodeMode "#r#"}
-                        -- We get a model
-                    }
-                , Cmd.none
-                )
-                ( update
-                    (ChangeMode (CodeMode ""))  -- Even though we sent nothing
-                    smallModel
-                )
-
-
-choosingBlockUpdates : () -> Expect.Expectation
-choosingBlockUpdates =
-    let
-       uiState = emptyModel.uiState
-    in
-        \() ->
-            Expect.equal
-                (
-                    { emptyModel
-                    | uiState =
-                        { uiState
-                        | mode = PlaceBlockMode
-                        , block = Just (Block Earth UpRight)
-                        }
-                    }
-                , Cmd.none
-                )
-                ( update
-                    (ChangeBlock (Block Earth UpRight))
-                    emptyModel
-                )
+        case msgsAndModels of
+            [] ->
+                []
+            (msg, model) :: mms ->
+                t initModel msg model :: expectUpdateGives (num + 1) model mms
