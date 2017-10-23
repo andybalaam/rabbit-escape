85e9524c49a67150331306656eea12dd4b918a99
Andy Balaam <andybalaam@artificialworlds.net>
2017-10-23 10:07:31 +0100

    Refactor renderer tests

--- level-editor/tests/WorldTextRenderTests.elm
+++ level-editor/tests/WorldTextRenderTests.elm
@@ -18,12 +18,83 @@ import WorldTextRender exposing (render)
 
 all : Test
 all =
-    describe "Tests of the world parser and manipulation"
-        [ test "Render empty world" renderEmptyWorld
-        , test "Render world with blocks" renderWorldWithBlocks
-        , test "Render world with rabbits" renderWorldWithRabbits
-        , test "Render 2 rabbits in same place" render2RabbitsInSamePlace
-        , test "Render rabbit on top of block" renderRabbitOnBlock
+    describe "Tests of the world renderer"
+        [ t "Render empty world"
+            [ [NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, NoBlock]
+            ]
+            []
+            [ "   "
+            , "   "
+            , "   "
+            ]
+
+        , t "Render world with blocks"
+            [ [NoBlock, NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, NoBlock, fltErth]
+            , [NoBlock, NoBlock, NoBlock, NoBlock]
+            , [fltErth, fltErth, fltErth, fltErth]
+            ]
+            []
+            [ "    "
+            , "   #"
+            , "    "
+            , "####"
+            ]
+
+        , t "Render world with rabbits"
+            [ [NoBlock, NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, NoBlock, fltErth]
+            , [NoBlock, NoBlock, NoBlock, NoBlock]
+            , [fltErth, fltErth, fltErth, fltErth]
+            ]
+            [ makeRabbot 0 1 Right
+            , makeRabbot 0 0 Left
+            , makeRabbit 2 1 Right
+            , makeRabbit 1 2 Left
+            ]
+            [ "y   "
+            , "t r#"
+            , " j  "
+            , "####"
+            ]
+
+        , t "Render 2 rabbits in same place"
+            [ [NoBlock, NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, NoBlock, NoBlock]
+            ]
+            [ makeRabbit 2 1 Right
+            , makeRabbit 2 1 Left
+            ]
+            [ "    "
+            , "  * "
+            , "    "
+            , "    "
+            , ":*=rj"
+            ]
+
+        , t "Render rabbit on top of block"
+            [ [NoBlock, NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, fltErth, fltMetl]
+            , [uprErth, NoBlock, NoBlock, NoBlock]
+            , [NoBlock, NoBlock, NoBlock, NoBlock]
+            ]
+            [ makeRabbit 2 1 Right
+            , makeRabbit 2 1 Left
+            , makeRabbit 3 1 Right
+            , makeRabbit 0 2 Left
+            ]
+            [ "    "
+            , "  **"
+            , "*   "
+            , "    "
+            , ":*=#rj"
+            , ":*=Mr"
+            , ":*=/j"
+            ]
         ]
 
 
@@ -55,110 +126,10 @@ rend blocks rabbits =
     renderToLines (makeWorld "tst" (makeBlockGrid blocks) rabbits)
 
 
--- ---
-
-renderEmptyWorld : () -> Expect.Expectation
-renderEmptyWorld =
-    \() ->
-        Expect.equal
-            [ "   "
-            , "   "
-            , "   "
-            ]
-            (rend
-                [ [NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, NoBlock]
-                ]
-                []
-            )
-
-
-renderWorldWithBlocks : () -> Expect.Expectation
-renderWorldWithBlocks =
-    \() ->
-        Expect.equal
-            [ "    "
-            , "   #"
-            , "    "
-            , "####"
-            ]
-            (rend
-                [ [NoBlock, NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, NoBlock, fltErth]
-                , [NoBlock, NoBlock, NoBlock, NoBlock]
-                , [fltErth, fltErth, fltErth, fltErth]
-                ]
-                []
-            )
-
-
-renderWorldWithRabbits : () -> Expect.Expectation
-renderWorldWithRabbits =
-    \() ->
-        Expect.equal
-            [ "y   "
-            , "t r#"
-            , " j  "
-            , "####"
-            ]
-            (rend
-                [ [NoBlock, NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, NoBlock, fltErth]
-                , [NoBlock, NoBlock, NoBlock, NoBlock]
-                , [fltErth, fltErth, fltErth, fltErth]
-                ]
-                [ makeRabbot 0 1 Right
-                , makeRabbot 0 0 Left
-                , makeRabbit 2 1 Right
-                , makeRabbit 1 2 Left
-                ]
-            )
-
-
-render2RabbitsInSamePlace : () -> Expect.Expectation
-render2RabbitsInSamePlace =
-    \() ->
-        Expect.equal
-            [ "    "
-            , "  * "
-            , "    "
-            , "    "
-            , ":*=rj"
-            ]
-            (rend
-                [ [NoBlock, NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, NoBlock, NoBlock]
-                ]
-                [ makeRabbit 2 1 Right
-                , makeRabbit 2 1 Left
-                ]
-            )
-
-
-renderRabbitOnBlock : () -> Expect.Expectation
-renderRabbitOnBlock =
-    \() ->
-        Expect.equal
-            [ "    "
-            , "  **"
-            , "*   "
-            , "    "
-            , ":*=#rj"
-            , ":*=Mr"
-            , ":*=/j"
-            ]
-            (rend
-                [ [NoBlock, NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, fltErth, fltMetl]
-                , [uprErth, NoBlock, NoBlock, NoBlock]
-                , [NoBlock, NoBlock, NoBlock, NoBlock]
-                ]
-                [ makeRabbit 2 1 Right
-                , makeRabbit 2 1 Left
-                , makeRabbit 3 1 Right
-                , makeRabbit 0 2 Left
-                ]
-            )
+t : String -> List (List Block) -> List Rabbit -> List String -> Test
+t desc blocks rabbits expected =
+    test
+        desc
+        ( \() ->
+            Expect.equal expected (rend blocks rabbits)
+        )
