0a3e04dff4f4342f2e38562e3ac937b930268c0e
colonelfazackerley <colonelfazackerley@users.noreply.github.com>
2018-10-16 10:01:03 +0100

    particles for falling water

---
+++ rabbit-escape-render/src/rabbitescape/render/WaterParticle.java
@@ -0,0 +1,82 @@
+package rabbitescape.render;
+
+import java.util.Random;
+import rabbitescape.engine.util.Position;
+import rabbitescape.engine.WaterRegion;
+
+public class WaterParticle
+{
+
+    private static final Random rand = new Random();
+    /** Change in opacity per animation step. */
+    public static final int alphaStepMagnitude = 32;
+    /** Acceleration due to gravity in cells per animation step squared. */
+    private final float gravity = 0.008f;
+    /** Damping: fraction of velocity retained per animation step. */
+    private final float damping = 0.95f;
+    
+    /** Coordinates within region. 0 to 1 is within cell*/
+    private float x, y, lastX, lastY;
+    /** Velocity in cells per animation step.*/
+    private float vx, vy;
+    /** Particles fade in and out. 0-255. */
+    public int alpha = alphaStepMagnitude;
+    public int alphaStep = alphaStepMagnitude;
+    /** The region this particle belongs too. */
+    private WaterRegion region;
+    
+    public WaterParticle(WaterRegion region)
+    {
+        this.region = region;
+        x = rand.nextFloat();
+        y = rand.nextFloat();
+        lastX = x; lastY = y;
+        vx = ( rand.nextFloat() - 0.5f ) / 32f ;
+        vy = ( rand.nextFloat() - 0.5f ) / 32f ;
+    }
+    
+    public boolean outOfRegion()
+    {
+        return !( x < 0f || y < 0f || x >= 1f || y >= 1f );
+    }
+
+    /**
+     * return position, scaled by f and offset. Also offset from
+     * region's origin to global origin.
+     */
+    private Vertex position(float x_, float y_, float tileSize, Vertex offset )
+    {
+        Position p = region.getPosition();
+        return new Vertex(p.x * tileSize + offset.x + x_ * tileSize,
+                          p.y * tileSize + offset.y + y_ * tileSize);
+    }
+    
+    /**
+     * return position, scaled by f and offset. Also offset from
+     * region's origin to global origin.
+     */
+    public Vertex position(float tileSize, Vertex offset)
+    {
+        return position(x, y, tileSize, offset);
+    }
+
+    public Vertex lastPosition(float tileSize, Vertex offset)
+    {
+        return position(lastX, lastY, tileSize, offset);
+    }
+    
+    /**
+     * Apply momentum and gravity and damping. Called once per animation step.
+     */
+    public void step()
+    {
+        // store previous position, so streaks can be drawn.
+        lastX = x; lastY = y;
+        // apply momentum
+        x += vx; y += vy;
+        // apply gravity
+        vy += gravity;
+        // apply damping to limit top speed
+        vx *= damping; vy *= damping;
+    }
+}
