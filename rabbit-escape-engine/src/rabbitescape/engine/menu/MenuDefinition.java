f4f8cc4c0766d3d11d4a76181a120bc4c3e17dd1
Andy Balaam <andybalaam@artificialworlds.net>
2019-01-26 15:50:06 +0000

    Add Water levelset to menu

--- rabbit-escape-engine/src/rabbitescape/engine/menu/MenuDefinition.java
+++ rabbit-escape-engine/src/rabbitescape/engine/menu/MenuDefinition.java
@@ -19,6 +19,7 @@ public class MenuDefinition
         new LevelSetInfo( "Small World", "06_small_world", null ),
         new LevelSetInfo( "Deja Vu", "07_dejavu", null ),
         new LevelSetInfo( "Rabbots", "08_rabbots", null ),
+        new LevelSetInfo( "Water", "09_water", null ),
         new LevelSetInfo( "Development", "development", null, true ),
         new LevelSetInfo( "Staging", "staging", null, true )
     );
