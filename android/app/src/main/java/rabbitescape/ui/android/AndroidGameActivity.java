c58afffeae19343a7fdfb68ca43758d108938ade
Andy Balaam <andybalaam@artificialworlds.net>
2024-11-06 14:15:59 +0000

    Remove unused Settings menu

--- android/app/src/main/java/rabbitescape/ui/android/AndroidGameActivity.java
+++ android/app/src/main/java/rabbitescape/ui/android/AndroidGameActivity.java
@@ -4,8 +4,6 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -319,28 +317,6 @@ public class AndroidGameActivity extends RabbitEscapeActivity
         }
 
         return -1;
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu( Menu menu )
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.game, menu );
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected( MenuItem item )
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if ( id == R.id.action_settings )
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected( item );
     }
 
     @Override
--- android/app/src/main/java/rabbitescape/ui/android/AndroidMenuActivity.java
+++ android/app/src/main/java/rabbitescape/ui/android/AndroidMenuActivity.java
@@ -236,26 +236,4 @@ public class AndroidMenuActivity extends RabbitEscapeActivity
         intent.putExtra( POSITION, shrunk( positions ) );
         return intent;
     }
-
-    @Override
-    public boolean onCreateOptionsMenu( android.view.Menu menu )
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate( R.menu.android_menu, menu );
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected( android.view.MenuItem item )
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if ( id == R.id.action_settings )
-        {
-            return true;
-        }
-        return super.onOptionsItemSelected( item );
-    }
 }
